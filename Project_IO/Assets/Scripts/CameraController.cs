744ee2bb58813390a24a0c80c04e55aaf3b93622
최정환 <cjh6220@gmail.com>
2019-10-20 01:31:39 +0900

    플레이어 컨트롤

---
+++ Project_IO/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*float playerXPos = player.transform.position.x;
+        float playerYPos = player.transform.position.y;
+        this.transform.position = new Vector3(playerXPos, playerYPos, -10);*/
+        Vector3 currentPos = new Vector3(transform.position.x, transform.position.y, -10f);
+        Vector3 targetPos = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
+
+        transform.position = Vector3.MoveTowards(currentPos, targetPos, 0.1f);
+    }
+}
---
+++ Project_IO/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject switchPibot;
+    public GameObject circle1;
+    public GameObject circle2;
+
+    public bool change = false;    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switchPibot.transform.Rotate(new Vector3(0, 0, -400) * Time.deltaTime);
+
+        if(Input.GetMouseButtonDown(0))
+        {   
+            ChangeFoot();
+        }
+    }
+
+    private void ChangeFoot()
+    {
+        if(change)
+        {
+            circle1.transform.SetParent(switchPibot.transform);
+            circle1.transform.localPosition = new Vector3(-2, 0, 0);
+            circle1.GetComponent<SpriteRenderer>().color = Color.red;
+
+            circle2.transform.SetParent(this.transform);
+            circle2.transform.localPosition = Vector3.zero;
+            circle2.GetComponent<SpriteRenderer>().color = Color.white;
+
+            Vector3 worldPos = switchPibot.transform.GetChild(0).position;
+            this.transform.position = worldPos;
+
+            change = false;
+        }
+        else
+        {
+            circle2.transform.SetParent(switchPibot.transform);
+            circle2.transform.localPosition = new Vector3(-2, 0, 0);
+            circle2.GetComponent<SpriteRenderer>().color = Color.white;
+
+            circle1.transform.SetParent(this.transform);
+            circle1.transform.localPosition = Vector3.zero;
+            circle1.GetComponent<SpriteRenderer>().color = Color.red;
+
+            Vector3 worldPos = switchPibot.transform.GetChild(0).position;
+            this.transform.position = worldPos;
+
+            change = true;
+        }
+    }
+
+
+}
