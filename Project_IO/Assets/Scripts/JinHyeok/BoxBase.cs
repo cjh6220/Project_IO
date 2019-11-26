6c2095c71f47fb2bb7b37c8dd512f03931b5053c
jinhueck <qorgh9184@gmail.com>
2019-11-26 20:25:32 +0900

    기초 틀 잡아놓은것

---
+++ Project_IO/Assets/Scripts/JinHyeok/BoxBase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BoxType
+{
+    Type_Tree = 0,
+    Type_Sliver = 1,
+    Type_Gold = 2,
+}
+
+public class BoxBase
+{
+    public BoxType boxType;
+    public int maxHit;
+    public int presentHit;
+    private List<ItemBase> itemList;
+
+    public BoxBase()
+    {
+        CreateItems();
+    }
+
+    private void CreateItems()
+    {
+        int totalCount = 0;
+        switch(boxType)
+        {
+            case BoxType.Type_Tree: totalCount = 1; break;
+            case BoxType.Type_Sliver: totalCount = 2; break;
+            case BoxType.Type_Gold: totalCount = 3; break;
+            default: Debug.LogError("Can't Create Item : This box doesn't have box type info"); return;
+        }
+        int presentCount = Random.Range(totalCount, totalCount + 2);
+
+        for (int i = 0; i < presentCount; i++)
+        {
+            ItemBase nItem = new ItemBase();
+            itemList.Add(nItem);
+        }
+    }
+
+    public void DropItems()
+    {
+
+    }
+}
---
+++ Project_IO/Assets/Scripts/JinHyeok/ItemBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBase
+{
+    public int itemType;    //아이템 종류
+    public int value;       //아이템이 보유한 값
+    public Texture boxTexture;
+
+    public ItemBase()
+    {
+
+    }
+
+    public void MoveItem()
+    {
+
+    }
+
+    public virtual void ApplyValue() { }    //값을 플레이어에게 적용할 수 있는 함수
+}
